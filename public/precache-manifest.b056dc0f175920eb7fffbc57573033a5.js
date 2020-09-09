50133a5a0cf13aebe6474e3edfe21e6883777c88
bangina <bbangina@gmail.com>
2020-09-09 16:13:41 +0900

    Initial commit

---
+++ public/precache-manifest.b056dc0f175920eb7fffbc57573033a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146b6305cd426530b4ef59f88026030d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f65e2fb73dbd1be2f468",
+    "url": "./static/css/2.3daec2b1.chunk.css"
+  },
+  {
+    "revision": "a7958f8b7ce1c6e3b7b1",
+    "url": "./static/css/main.16a432da.chunk.css"
+  },
+  {
+    "revision": "f65e2fb73dbd1be2f468",
+    "url": "./static/js/2.9341a3f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9341a3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7958f8b7ce1c6e3b7b1",
+    "url": "./static/js/main.f839e41a.chunk.js"
+  },
+  {
+    "revision": "e7cbccf78511bea514e3",
+    "url": "./static/js/runtime-main.a10ff605.js"
+  }
+]);
